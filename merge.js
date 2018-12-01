8720128e76a5ee0f5848743c33528fcf4861b79e
yutopp <yutopp@gmail.com>
2018-12-02 05:01:32 +0900

    Show progress when merging

--- merge.js
+++ merge.js
@@ -2,9 +2,11 @@ const glob = require("glob-fs")({ gitignore: false });
 const fs = require("fs");
 
 const jsonFiles = glob.readdirSync("./_items/*.json");
-const jsons = jsonFiles.map(jsonFile =>
-  JSON.parse(fs.readFileSync(jsonFile, "utf8"))
-);
+const jsons = jsonFiles.map(jsonFile => {
+  console.error(`Loading: ${jsonFile}`);
+  const json = fs.readFileSync(jsonFile, "utf8");
+  return JSON.parse(json);
+});
 const items = jsons.flat();
 
 console.log(JSON.stringify({ items: items }, null, "  "));
